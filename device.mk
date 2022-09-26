906ba625d70473d6073b89af3555029bba869985
EduardoA3677 <ealvarado261500@gmail.com>
2022-09-25 20:36:17 -0500

    a12

--- device.mk
+++ device.mk
@@ -72,6 +72,3 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES_ENG += \
 	tzdata_twrp
 
-# Apex libraries
-PRODUCT_COPY_FILES += \
-	$(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
